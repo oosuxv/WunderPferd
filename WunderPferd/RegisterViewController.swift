514d28e5460b7a89433aff65abd6e0e550052e5c
Vadim Gorodilov <vadim.gorodilov@gmail.com>
2022-04-15 15:15:27 +0700

    add interface and input handling

---
+++ WunderPferd/RegisterViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RegisterViewController.swift
+//  WunderPferd
+//
+//  Created by Vadim Gorodilov on 15.04.2022.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+        
+        textFields.forEach { field in
+            field.delegate = self
+            if field == textFields.last {
+                field.returnKeyType = .done
+            } else {
+                field.returnKeyType = UIReturnKeyType.next
+            }
+        }
+        textFields.first?.becomeFirstResponder()
+
+    }
+    
+    @IBOutlet var textFields: [UITextField]!
+  
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ currentField: UITextField) -> Bool {
+        if textFields.last == currentField {
+            currentField.resignFirstResponder()
+            return true
+        }
+        if let index = textFields.firstIndex(where: {textField in textField == currentField}) {
+            textFields[index + 1].becomeFirstResponder()
+            if index == textFields.count - 2 {
+                
+            }
+        }
+        return true
+    }
+}

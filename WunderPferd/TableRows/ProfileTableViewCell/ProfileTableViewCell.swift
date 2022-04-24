2e96e37d1e2ebfa762833e8415a5024dab1b18d8
Vadim Gorodilov <vadim.gorodilov@gmail.com>
2022-04-24 13:40:26 +0700

    remove unused methods

--- WunderPferd/TableRows/ProfileTableViewCell/ProfileTableViewCell.swift
+++ WunderPferd/TableRows/ProfileTableViewCell/ProfileTableViewCell.swift
@@ -11,15 +11,4 @@ class ProfileTableViewCell: UITableViewCell {
     
     @IBOutlet weak var attribute: UILabel!
     @IBOutlet weak var value: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 }

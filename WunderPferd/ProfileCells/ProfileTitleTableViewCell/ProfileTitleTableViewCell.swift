cca4d55b9757acec084315761d4a6b0b4ff9b1ca
Vadim Gorodilov <vadim.gorodilov@gmail.com>
2022-04-28 17:51:58 +0700

    move profile view to table

---
+++ WunderPferd/ProfileCells/ProfileTitleTableViewCell/ProfileTitleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileTitleTableViewCell.swift
+//  WunderPferd
+//
+//  Created by Vadim Gorodilov on 28.04.2022.
+//
+
+import UIKit
+
+class ProfileTitleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var wideImageView: UIImageView!
+    @IBOutlet weak var circledImageView: UIImageView!
+    @IBOutlet weak var loginLabel: UILabel!
+}
--- WunderPferd/ProfileViewController.swift
+++ WunderPferd/ProfileViewController.swift
@@ -11,6 +11,7 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    
     var source: [(String, String)] = [
             ("first name", "Bob"),
             ("last name", "Bobson"),
@@ -22,30 +23,50 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
-        let nib = UINib(nibName: ProfileDataTableViewCell.className, bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: ProfileDataTableViewCell.className)
+
+        tableView.register(UINib(nibName: ProfileTitleTableViewCell.className, bundle: nil),
+                           forCellReuseIdentifier: ProfileTitleTableViewCell.className)
+        tableView.register(UINib(nibName: ProfileDataTableViewCell.className, bundle: nil),
+                           forCellReuseIdentifier: ProfileDataTableViewCell.className)
     }
 }
 
 extension ProfileViewController: UITableViewDelegate {
-    // TODO: realize
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
 }
 
 extension ProfileViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return source.count
+        section == 0 ? 1 : source.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileDataTableViewCell.className) as? ProfileDataTableViewCell else {
-            return UITableViewCell()
+        print("\(indexPath.section) \(indexPath.row)")
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileTitleTableViewCell.className) as? ProfileTitleTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.circledImageView.layer.borderWidth = 5
+            cell.circledImageView.layer.masksToBounds = false
+            cell.circledImageView.layer.borderColor = UIColor.white.cgColor
+            cell.circledImageView.layer.cornerRadius = cell.circledImageView.frame.height / 2
+            cell.circledImageView.clipsToBounds = true
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileDataTableViewCell.className) as? ProfileDataTableViewCell else {
+                return UITableViewCell()
+            }
+            let tuple = source[indexPath.row]
+            cell.attribute.text = tuple.0
+            cell.value.text = tuple.1
+            return cell
         }
-        let tuple = source[indexPath.row]
-        cell.attribute.text = tuple.0
-        cell.value.text = tuple.1
-        return cell
     }
 }

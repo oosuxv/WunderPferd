93a3fb8041f75765fc163376a10cfcfba7d15810
Vadim Gorodilov <vadim.gorodilov@gmail.com>
2022-05-25 03:54:35 +0700

    refactor: simplify ServiceLocator

--- WunderPferd/Services/ServiceLocator.swift
+++ WunderPferd/Services/ServiceLocator.swift
@@ -8,24 +8,28 @@
 import Foundation
 
 final class ServiceLocator {
-
-    static let shared = ServiceLocator() // Singleton
-
-    var storageManager: StorageManager
-    var profileNetworkManager: ProfileNetworkManager
-    var profileImageManager: ProfileImageManager
-    var profileDataInteractor: ProfileDataInteractor
-    var rickNetworkManager: RickNetworkManager
     
-    init(storageManager: StorageManager = KeychainDefaultsStorageManager(),
-         profileNetworkManager: ProfileNetworkManager = NetworkManager(),
-         profileImageManager: ProfileImageManager = FileProfileImageManager(),
-         rickNetworkManager: RickNetworkManager = NetworkManager()
-    ) {
-        self.storageManager = storageManager
-        self.profileNetworkManager = profileNetworkManager
-        self.profileImageManager = profileImageManager
-        self.rickNetworkManager = rickNetworkManager
-        self.profileDataInteractor = DefaultProfileDataInteractor(storageManager: storageManager, networkManager: profileNetworkManager, profileImageManager: profileImageManager)
+    static func storageManager() -> StorageManager {
+        return KeychainDefaultsStorageManager()
+    }
+
+    static func profileNetworkManager() -> ProfileNetworkManager {
+        return NetworkManager()
+    }
+    
+    static func profileImageManager() -> ProfileImageManager {
+        return FileProfileImageManager()
+    }
+    
+    static func profileDataInteractor() -> ProfileDataInteractor {
+        return DefaultProfileDataInteractor(
+            storageManager: storageManager(),
+            networkManager: profileNetworkManager(),
+            profileImageManager: profileImageManager()
+        )
+    }
+    
+    static func rickNetworkManager() -> RickNetworkManager {
+        return NetworkManager()
     }
 }
